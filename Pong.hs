88e33c343b51671d99780d7891f0c7965299aa57
Samuel Gélineau <gelisam+github@gmail.com>
2015-12-10 21:17:13 -0500

    my first ffi

--- Pong.hs
+++ Pong.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 import Haste
+import Haste.Foreign
 
+
+console_log :: JSString -> IO ()
+console_log = ffi "(function(x) {console.log(x)})"
 
 main :: IO ()
 main = do
   putStrLn "typechecks."
+  console_log "it talks!"
