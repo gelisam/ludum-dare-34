71131dccf28d8da82d7a7b81d8d5ae7ecf9fd940
Samuel Gélineau <gelisam+github@gmail.com>
2015-12-11 22:59:44 -0500

    remove the examples which have been reimplemented in Haskell

--- src/Main.hs
+++ src/Main.hs
@@ -7,21 +7,6 @@ import SpriteJS
 import WindowJS
 
 
-visual_guide :: IO ()
-visual_guide = do
-    -- create the DOM element expected by visual_guide.js
-    div <- newElem "ol" `with` [ attr "id"        =: "exec_list"
-                               , style "position" =: "absolute"
-                               , style "left"     =: "400px"
-                               ]
-    appendChild documentBody div
-    
-    ffi "visual_guide"
-
-
-console_log :: JSString -> IO ()
-console_log = ffi "(function(x) {console.log(x)})"
-
 main :: IO ()
 main = do
     putStrLn "it begins!"
@@ -36,4 +21,4 @@ main = do
       setSpriteOpacity bird 0.8
       updateSprite bird
     
-    console_log "done."
+    putStrLn "done."
