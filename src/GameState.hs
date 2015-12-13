83861715eeadad2069ffa1cd0634cd18a3036aca
Samuel Gélineau <samuel.gelineau@keatext.com>
2015-12-13 11:38:38 -0500

    the world zipper algorithm
    to have one global state with everything in the world, while limiting
    the number of sprites on screen.

--- src/GameState.hs
+++ src/GameState.hs
@@ -1,5 +1,7 @@
 module GameState where
 
+import Control.Arrow
+
 import Entities
 import Looping
 import Centered
@@ -22,3 +24,38 @@ data GameState = GameState
   , onScreenEntities :: [OnScreenEntity]
   , missedEntities   :: [OffScreenEntity]
   }
+
+-- move entities between the on-screen and off-screen buffers.
+-- 
+-- For Ordering, GT means entirely above the screen, EQ means at least partially on screen,
+-- and LT means entirely below the screen.
+-- 
+-- Entities in the first buffer (entities below the screen) should be order from top to bottom,
+-- entities in the second buffer (entities on the screen) should be ordered from bottom to top,
+-- entities in the last buffer (entities above the screen) should be ordered from bottom to top.
+shuffleZipper :: Monad m
+              => (h -> offScreen -> Ordering)
+              -> (h -> onScreen  -> Ordering)
+              -> (offScreen -> m onScreen)
+              -> (onScreen -> m offScreen)
+              -> h
+              -> ([offScreen], [onScreen], [offScreen])
+              -> m ([offScreen], [onScreen], [offScreen])
+shuffleZipper isOffVisible isOnVisible putOnScreen takeOffScreen h (below, current, above) = do
+    newlyBelow' <- mapM takeOffScreen newlyBelow
+    newlyAbove' <- mapM takeOffScreen newlyAbove
+    belowCurrent' <- mapM putOnScreen belowCurrent
+    aboveCurrent' <- mapM putOnScreen aboveCurrent
+    
+    let below'   = reverse newlyBelow' ++ stillBelow
+    let current' = reverse belowCurrent' ++ stillCurrent ++ aboveCurrent'
+    let above'   = newlyAbove' ++ stillAbove
+    
+    return (below', current', above')
+  where
+    (belowCurrent, stillBelow) = span (isOffVisible h >>> (/= LT)) below
+    (aboveCurrent, stillAbove) = span (isOffVisible h >>> (/= GT)) above
+    
+    newlyBelow   = filter (isOnVisible h >>> (== LT)) current
+    stillCurrent = filter (isOnVisible h >>> (== EQ)) current
+    newlyAbove   = filter (isOnVisible h >>> (== GT)) current
