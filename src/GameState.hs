6a1b6e142584ec1909501cc58800d252c612e177
Samuel Gélineau <samuel.gelineau@keatext.com>
2015-12-13 14:21:15 -0500

    refactor main into a game loop

--- src/GameState.hs
+++ src/GameState.hs
@@ -1,15 +1,27 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
 module GameState where
 
 import Control.Arrow
+import Control.Monad
+import Data.IORef
+import Haste
+import Haste.DOM
 import Haste.Prim
+import Haste.Foreign
+import Text.Printf
 
+import Animated
+import Animation
 import Backgrounds
-import Entities
-import Looping
+import Constants
 import Centered
+import Entities
+import JSRef
+import Looping
+import Random
 import Scaled
 import SpriteJS
+import WindowJS
 
 
 data PlayerStatus
@@ -19,10 +31,18 @@ data PlayerStatus
 
 
 data GameState = GameState
-  { playerStatus     :: PlayerStatus
-  , playerSprite     :: Looping (Scaled (Centered NormalSprite))
-  , gameHeight       :: Double
-  , bestGameHeight   :: Double
+  { gameScene      :: Ptr Scene
+  , gameBackLayer1 :: Ptr Layer
+  , gameBackLayer2 :: Ptr Layer
+  , gameFrontLayer :: Ptr Layer
+  
+  , playerStatus   :: PlayerStatus
+  , playerSprite   :: Looping (Scaled (Centered NormalSprite))
+  , gameHeight     :: Double
+  , bestGameHeight :: Double
+  
+  , scoreSprite :: NormalSprite
+  , gameScore   :: Double
   
   , entitiesBelow   :: [OffScreenEntity]
   , currentEntities :: [OnScreenEntity]
@@ -32,6 +52,131 @@ data GameState = GameState
   , currentBackgrounds :: [OnScreenBackground]
   , backgroundsAbove   :: [OffScreenBackground]
   }
+
+
+computeSeconds :: Int -> Double
+computeSeconds ticks = fromIntegral ticks / fps
+
+birdAnimation :: OffScreenBird -> Animation ((Double, Double), Bool)
+birdAnimation offScreenBird = fmap go xAndIsGoingLeft
+  where
+    xAndIsGoingLeft :: Animation (Double, Bool)
+    xAndIsGoingLeft = bounce (birdWidth / 2, game_width - birdWidth / 2)
+                    $ flip linear birdPixelsPerSecond
+                    $ birdInitialX offScreenBird
+    
+    go :: (Double,Bool) -> ((Double, Double), Bool)
+    go (x,isGoingLeft) = ((x,y),isFlipped)
+      where
+        y = birdInitialY offScreenBird
+        isFlipped = not isGoingLeft
+
+
+newPlayerSprite :: CanHoldSprite a => a -> IO (Looping (Scaled (Centered NormalSprite)))
+newPlayerSprite parent = do
+    sprite <- newLooping parent playerImageWidth 1 5
+            $ newScaled 1.0
+            $ newCentered playerImageWidth playerImageHeight
+            $ newSprite parent "img/up.png"
+    writeJSRef (spriteXScale sprite) (-1)
+    return sprite
+
+newBirdSprite :: CanHoldSprite a
+              => a
+              -> OffScreenBird
+              -> IO (Animated (Looping (Scaled (Centered NormalSprite))))
+newBirdSprite parent offScreenBird = newDirectionalMoving (birdAnimation offScreenBird)
+                                   $ newLooping parent birdImageWidth 11 5
+                                   $ newScaled birdScale
+                                   $ newCentered birdImageWidth birdImageHeight
+                                   $ newSprite parent "img/flying-enemy.png"
+
+-- TODO: use a random balloon image instead
+newBalloonSprite :: CanHoldSprite a => a -> IO NormalSprite
+newBalloonSprite parent = do
+    balloon <- newTopLeftAligned 20 20
+             $ newEmptySprite parent
+    
+    dom <- getDom balloon
+    set dom [style "border" =: "2px solid #880000"]
+    set dom [style "border-radius" =: "10px"]
+    set dom [style "background-color" =: "red"]
+    
+    return balloon
+
+
+newGameState :: IO GameState
+newGameState = do
+    scene <- newScene game_width game_height True
+    back2 <- newLayer scene "back-2"
+    back1 <- newLayer scene "back-1"
+    front <- newLayer scene "front"
+    
+    
+    scoreSprite <- newTopLeftAligned 200 100
+           $ newEmptySprite front
+    writeJSRef (spritePosition scoreSprite) (20, 20)
+    
+    dom <- getDom scoreSprite
+    set dom [attr "id" =: "score"]
+    
+    
+    let mountainImage = "img/mountain-shadows.png"
+    let mountainAnimation = delayed 10
+                          $ linear 0 9
+    let offScreenMountain = OffScreenParallaxLayer mountainImage mountainAnimation
+    mountainSprite <- newParallax mountainAnimation
+                    $ newTopLeftAligned 640 920
+                    $ newSprite back2 mountainImage
+    let onScreenMountain = OnScreenParallaxLayer mountainSprite offScreenMountain
+    
+    let buildingImage = "img/city-zoomed-in.png"
+    let buildingAnimation = linear (920 - 2856) 56
+    let offScreenBuilding = OffScreenParallaxLayer buildingImage buildingAnimation
+    buildingSprite <- newParallax buildingAnimation
+                    $ newTopLeftAligned 640 2856
+                    $ newSprite back1 "img/city-zoomed-in.png"
+    let onScreenBuilding = OnScreenParallaxLayer buildingSprite offScreenBuilding
+    
+    let buildingShadowImage = "img/city-shadow.png"
+    let buildingShadowAnimation = delayed 10
+                                $ linear 0 18
+    let offScreenBuildingShadow = OffScreenParallaxLayer buildingShadowImage buildingShadowAnimation
+    buildingShadowSprite <- newParallax buildingShadowAnimation
+                          $ newTopLeftAligned 640 920
+                          $ newSprite back2 buildingShadowImage
+    let onScreenBuildingShadow = OnScreenParallaxLayer buildingShadowSprite offScreenBuildingShadow
+    
+    let offScreenBird = (OffScreenBird 0 0)
+    birdSprite <- newBirdSprite front offScreenBird
+    let onScreenBird = OnScreenBird birdSprite offScreenBird
+    
+    playerSprite <- newPlayerSprite front
+    writeJSRef (spritePosition playerSprite) (40, 200)
+    
+    return $ GameState
+      { gameScene      = scene
+      , gameBackLayer1 = back1
+      , gameBackLayer2 = back2
+      , gameFrontLayer = front
+      
+      , playerStatus     = Floating 1
+      , playerSprite     = playerSprite
+      , gameHeight       = 0
+      , bestGameHeight   = 0
+      
+      , entitiesBelow   = []
+      , currentEntities = [ BirdOn onScreenBird
+                          ]
+      , entitiesAbove   = []
+      
+      , backgroundsBelow   = []
+      , currentBackgrounds = [ ParallaxOn onScreenMountain
+                             , ParallaxOn onScreenBuilding
+                             , ParallaxOn onScreenBuildingShadow
+                             ]
+      , backgroundsAbove   = []
+      }
 
 -- move game objects between the on-screen and off-screen buffers.
 -- 
@@ -68,8 +213,43 @@ shuffleZipper isOffVisible isOnVisible putOnScreen takeOffScreen h (below, curre
     stillCurrent = filter (isOnVisible h >>> (== EQ)) current
     newlyAbove   = filter (isOnVisible h >>> (== GT)) current
 
+nextGameState :: Double -> Double -> Double -> Ptr Ticker -> GameState -> IO GameState
+nextGameState t h a ticker (GameState {..}) = do
+    return $ GameState
+      { playerStatus   = playerStatus'
+      , playerSprite   = playerSprite'
+      , gameHeight     = gameHeight'
+      , bestGameHeight = bestGameHeight'
+      
+      , entitiesBelow   = entitiesBelow'
+      , currentEntities = currentEntities'
+      , entitiesAbove   = entitiesAbove'
+      
+      , backgroundsBelow   = backgroundsBelow'
+      , currentBackgrounds = currentBackgrounds'
+      , backgroundsAbove   = backgroundsAbove'
+      }
+  where
+    playerStatus'   = playerStatus
+    playerSprite'   = playerSprite
+    gameHeight'     = gameHeight
+    bestGameHeight' = bestGameHeight
+    
+    entitiesBelow'   = entitiesBelow
+    currentEntities' = currentEntities
+    entitiesAbove'   = entitiesAbove
+    
+    backgroundsBelow'   = backgroundsBelow
+    currentBackgrounds' = currentBackgrounds
+    backgroundsAbove'   = backgroundsAbove
+    
+
 drawGameState :: Double -> Double -> Double -> Ptr Ticker -> GameState -> IO ()
 drawGameState t h a ticker (GameState {..}) = do
     updateSprite playerSprite (ticker, ())
     mapM_ (drawOnScreenEntity     t h a ticker) currentEntities
     mapM_ (drawOnScreenBackground t h a ticker) currentBackgrounds
+    
+    -- throws an exception for some reason?
+    --dom <- getDom scoreSprite
+    --set dom ["innerHTML" =: printf "Score %d" (round gameScore :: Int)]
--- src/Main.hs
+++ src/Main.hs
@@ -3,8 +3,8 @@ import Control.Arrow
 import Control.Monad
 import Data.IORef
 import Haste
-import Haste.DOM
 import Haste.Foreign
+import Haste.Prim
 import Text.Printf
 
 import Animated
@@ -21,146 +21,26 @@ import SpriteJS
 import WindowJS
 
 
-computeSeconds :: Int -> Double
-computeSeconds ticks = fromIntegral ticks / fps
-
-birdAnimation :: OffScreenBird -> Animation ((Double, Double), Bool)
-birdAnimation offScreenBird = fmap go xAndIsGoingLeft
-  where
-    xAndIsGoingLeft :: Animation (Double, Bool)
-    xAndIsGoingLeft = bounce (birdWidth / 2, game_width - birdWidth / 2)
-                    $ flip linear birdPixelsPerSecond
-                    $ birdInitialX offScreenBird
-    
-    go :: (Double,Bool) -> ((Double, Double), Bool)
-    go (x,isGoingLeft) = ((x,y),isFlipped)
-      where
-        y = birdInitialY offScreenBird
-        isFlipped = not isGoingLeft
-
-
-newPlayerSprite :: CanHoldSprite a => a -> IO (Looping (Scaled (Centered NormalSprite)))
-newPlayerSprite parent = do
-    sprite <- newLooping parent playerImageWidth 1 5
-            $ newScaled 1.0
-            $ newCentered playerImageWidth playerImageHeight
-            $ newSprite parent "img/up.png"
-    writeJSRef (spriteXScale sprite) (-1)
-    return sprite
-
-newBirdSprite :: CanHoldSprite a
-              => a
-              -> OffScreenBird
-              -> IO (Animated (Looping (Scaled (Centered NormalSprite))))
-newBirdSprite parent offScreenBird = newDirectionalMoving (birdAnimation offScreenBird)
-                                   $ newLooping parent birdImageWidth 11 5
-                                   $ newScaled birdScale
-                                   $ newCentered birdImageWidth birdImageHeight
-                                   $ newSprite parent "img/flying-enemy.png"
-
--- TODO: use a random balloon image instead
-newBalloonSprite :: CanHoldSprite a => a -> IO NormalSprite
-newBalloonSprite parent = do
-    balloon <- newTopLeftAligned 20 20
-             $ newEmptySprite parent
-    
-    dom <- getDom balloon
-    set dom [style "border" =: "2px solid #880000"]
-    set dom [style "border-radius" =: "10px"]
-    set dom [style "background-color" =: "red"]
-    
-    return balloon
-
-
 main :: IO ()
 main = do
-    scene <- newScene game_width game_height True
+    initialGameState <- newGameState
+    gameStateRef <- newIORef initialGameState
+    let scene = gameScene initialGameState
     loadImages scene ["img/up.png", "img/flying-enemy.png"] $ do
-      back2 <- newLayer scene "back-2"
-      back <- newLayer scene "back-1"
-      front <- newLayer scene "front"
-
---      city <- newTopLeftAligned 640 797
---            $ newSprite front "img/city-zoomed-out.png"
---      writeJSRef (spritePosition city) (0,123)
---      updateSprite city ()
-      mountain <- newParallax (delayed 10 $ linear 0 9)
-                $ newTopLeftAligned 640 920
-                $ newSprite back2 "img/mountain-shadows.png"
-      building <- newParallax (linear (920 - 2856) 56)
-                $ newTopLeftAligned 640 2856
-                $ newSprite back "img/city-zoomed-in.png"
-      building_shadow <- newParallax (delayed 10 $ linear 0 18)
-                       $ newTopLeftAligned 640 920
-                       $ newSprite back2 "img/city-shadow.png"
-
-      
-      bird <- newBirdSprite front (OffScreenBird 0 0)
-      
-      score <- newTopLeftAligned 200 100
-             $ newEmptySprite front
-      writeJSRef (spritePosition score) (20, 20)
-      updateSprite score ()
-      
-      dom <- getDom score
-      set dom [attr "id" =: "score"]
-      
-      player <- newPlayerSprite front
-      writeJSRef (spritePosition player) (40, 200)
-      
-      game_state_ref <- newIORef $ GameState
-        { playerStatus     = Floating 1
-        , playerSprite     = player
-        , gameHeight       = 0
-        , bestGameHeight   = 0
-        
-        , entitiesBelow   = []
-        , currentEntities = []
-        , entitiesAbove   = []
-        
-        , backgroundsBelow   = []
-        , currentBackgrounds = []
-        , backgroundsAbove   = []
-        }
-      
-      player_xv_ref <- newIORef 2.5
-      score_count_ref <- newIORef 0.0
-      
-      ticker <- newTicker scene fps $ \ticker -> do
-        ticks <- getCurrentTick ticker
-        let t = computeSeconds ticks
-        let h = 56 * t  -- for now, the screen rises 1 unit per second. Wait, is that 1 pixel?
-        let a = h  -- for now, the character ages at the same speed as the screen rises.
-        
-        updateSprite bird (t, h, a, (ticker, ()))
-        
-        -- Update first parallax layer
-        updateSprite building (t, h, a, ())
-
-        -- debugging to estimate cues
-        print t
-
-        updateSprite building_shadow (t, h, a, ())
-
-        updateSprite mountain (t, h, a, ())
-        
-        player_xv <- readIORef player_xv_ref
-        score_count <- readIORef score_count_ref
-        
-        modifyJSRef (spriteYVelocity player) (+ gravity)
-        applyVelocity player
-        
-        updateSprite player (ticker, ())
-
-        dom <- getDom score
-        score_count <- readIORef score_count_ref
-        set dom ["innerHTML" =: printf "Score %d" (round score_count :: Int)]
-        
-        y <- readJSRef (spriteYPosition player)
-        when (y > game_height) $ do
-          pauseTicker ticker
-          alert "Game over"
-        
-        modifyIORef player_xv_ref (+ 0.002)
-        modifyIORef score_count_ref (+ 0.08)
+      ticker <- newTicker scene fps (gameLoop gameStateRef)
       runTicker ticker
+
+gameLoop :: IORef GameState -> Ptr Ticker -> IO ()
+gameLoop gameStateRef ticker = do
+    gameState <- readIORef gameStateRef
+    
+    ticks <- getCurrentTick ticker
+    let t = computeSeconds ticks
+    let h = 56 * t  -- for now, the screen rises 1 unit per second. Wait, is that 1 pixel?
+    let a = h  -- for now, the character ages at the same speed as the screen rises.
+    
+    gameState' <- nextGameState t h a ticker gameState
+    drawGameState t h a ticker gameState'
+    
+    writeIORef gameStateRef gameState'
+    ticks `seq` t `seq` h `seq` a `seq` return ()
