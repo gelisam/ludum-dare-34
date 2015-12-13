fbb68ff97f4e5434f5338a0649bd6ae1ff2acdd6
Samuel Gélineau <samuel.gelineau@keatext.com>
2015-12-13 18:15:51 -0500

    mapMaybeM

--- src/Control/Monad/Extra.hs
+++ src/Control/Monad/Extra.hs
@@ -8,3 +8,11 @@ orM (action:actions) = do
     if r
     then return True
     else orM actions
+
+mapMaybeM :: Monad m => (a -> m (Maybe b)) -> [a] -> m [b]
+mapMaybeM _ [] = return []
+mapMaybeM check (x:xs) = do
+    r <- check x
+    case r of
+      Nothing ->           mapMaybeM check xs
+      Just y  -> (y :) <$> mapMaybeM check xs
