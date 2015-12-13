c05f6655e4b465aaacf414aa07b2e4054c0e390a
Samuel Gélineau <samuel.gelineau@keatext.com>
2015-12-13 11:08:46 -0500

    reimplement the Random module to avoid mysterious 'uncaught <#T2>' errors'

--- src/Random.hs
+++ src/Random.hs
@@ -1,37 +1,37 @@
 -- Haste's documentation says to use the random module, but doing so gives the error
 -- "ghczuwrapperZC0ZCbaseZCSystemziCPUTimeZCgetrusage is not defined" at runtime.
 -- 
--- Haste implements a few random primitives, but they're not as flexible as the random
--- module, so I'm reimplementing the random module on top of Haste's primitives.
+-- Haste implements a few random primitives, but is missing the primitives I've
+-- reimplemented here.
 module Random where
 
 import Data.IORef
 import Data.Tuple
-import qualified Haste as H
+import Haste
 import System.IO.Unsafe
-import qualified System.Random as R
 
 
-{-# NOINLINE stdGenRef #-}
-stdGenRef :: IORef R.StdGen
-stdGenRef = unsafePerformIO $ do
-    seed <- H.newSeed
-    -- (minBound, maxBound) causes an exception to be thrown
-    newIORef $ R.mkStdGen $ fst $ H.randomR (0, maxBound) seed
+data RandomState = RandomState
+  { currentSeed :: Seed
+  , currentSkipRate :: Int
+  }
 
-withStdGen :: (R.StdGen -> (a, R.StdGen)) -> IO a
-withStdGen f = atomicModifyIORef' stdGenRef (swap . f)
+{-# NOINLINE randomStateRef #-}
+randomStateRef :: IORef RandomState
+randomStateRef = unsafePerformIO $ do
+    seed <- newSeed
+    newIORef (RandomState seed 1)
 
 
-randomRIO :: R.Random a => (a, a) -> IO a
-randomRIO = withStdGen . R.randomR
+randomRIO :: Random a => (a, a) -> IO a
+randomRIO range = do
+    RandomState seed skipRate <- readIORef randomStateRef
+    let (x, seed') = randomR range seed
+    writeIORef randomStateRef (RandomState seed' skipRate)
+    return x
 
-randomIO :: R.Random a => IO a
-randomIO = withStdGen R.random
-
-randomIOs :: R.Random a => IO [a]
-randomIOs = do
-    x <- randomIO
-    g <- readIORef stdGenRef
-    let xs = R.randoms g
-    return (x:xs)
+randomRIOs :: Random a => (a, a) -> IO [a]
+randomRIOs range = do
+    independentInt <- randomRIO (0, maxBound)
+    let independentSeed = mkSeed independentInt
+    return $ randomRs range independentSeed
