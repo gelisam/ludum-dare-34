3a1cfde1c6caef4aa44ea42126bf6b2e7188b7c4
Samuel Gélineau <samuel.gelineau@keatext.com>
2015-12-13 11:40:21 -0500

    move constants to a separate file

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,7 @@ import Text.Printf
 
 import Animated
 import Animation
+import Constants
 import Centered
 import Entities
 import GameState
@@ -18,56 +19,6 @@ import Random
 import Scaled
 import SpriteJS
 import WindowJS
-
-
-game_width :: Num a => a
-game_width = 640
-
-game_height :: Num a => a
-game_height = 920
-
-gravity :: Double
-gravity = 0 -- 0.5
-
-fps :: Num a => a
-fps = 25
-
-playerPixelsPerSecond :: Num a => a
-playerPixelsPerSecond = 100
-
-playerScale :: Double
-playerScale = 1
-
-playerImageWidth :: Num a => a
-playerImageWidth = 117
-
-playerImageHeight :: Num a => a
-playerImageHeight = 300
-
-playerWidth :: Double
-playerWidth = playerScale * playerImageWidth
-
-playerHeight :: Double
-playerHeight = playerScale * playerImageHeight
-
-
-birdPixelsPerSecond :: Num a => a
-birdPixelsPerSecond = 100
-
-birdScale :: Double
-birdScale = 0.8
-
-birdImageWidth :: Num a => a
-birdImageWidth = 128
-
-birdImageHeight :: Num a => a
-birdImageHeight = 128
-
-birdWidth :: Double
-birdWidth = birdScale * birdImageWidth
-
-birdHeight :: Double
-birdHeight = birdScale * birdImageHeight
 
 
 computeSeconds :: Int -> Double
