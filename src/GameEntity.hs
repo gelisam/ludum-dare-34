53ed7c8f7c97fc20ff8bfcd807f5f10d2e94c3fb
Samuel Gélineau <gelisam+github@gmail.com>
2015-12-14 09:14:58 -0500

    generic implementation of isVisible

--- src/GameEntity.hs
+++ src/GameEntity.hs
@@ -1,14 +1,28 @@
 {-# LANGUAGE FunctionalDependencies #-}
 module GameEntity where
 
+import Control.Arrow
+
 import Globals
+import Constants
 
 
 class GameEntity on off | on  -> off
                         , off -> on
   where
-    ofScreen      :: on -> off
-    verticalSize  :: off -> (Double, Double)
+    offScreen     :: on -> off
+    verticalRange :: off -> (Double, Double)
     
     putOnScreen   :: Globals -> off -> IO on
     takeOffScreen :: on -> IO off
+
+isVisible :: GameEntity on off => Double -> off -> Ordering
+isVisible screenY off
+  | lo < screenY               = GT
+  | hi > screenY + game_height = LT
+  | otherwise                  = EQ
+  where
+    (lo, hi) = verticalRange off
+
+isStillVisible :: GameEntity on off => Double -> on -> Ordering
+isStillVisible screenY = offScreen >>> isVisible screenY
